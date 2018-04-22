6c8f761b70b6fa87956f8e69b2deab34d9b002ea
Guilherme Carvalho <guilherme_230308@hotmail.com>
2018-04-21 22:40:19 -0300

    Projeto teste criado

---
+++ src/main/java/com/testes/TestecursoApplication.java
@@ -0,0 +1,12 @@
+package com.testes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestecursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestecursoApplication.class, args);
+	}
+}
