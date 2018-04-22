03433b263b605ce96275843b08a673660de6bb9c
Guilherme Carvalho <guilherme_230308@hotmail.com>
2018-04-21 23:09:23 -0300

    Criando classe de cominio

--- src/main/java/com/testes/resources/CategoriaResource.java
+++ src/main/java/com/testes/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.testes.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.testes.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest Funcionando";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		List<Categoria> lista =  new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat1);
+		
+		return lista;
 	}
 }
